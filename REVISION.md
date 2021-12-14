f8692ccc80df1e367c7b5607f8524e204e8ce17b
Edu Martínez <emartinezr161@outlook.com>
2021-12-14 16:10:08 +0100

    borrado material de ejemplo, creada migracion para v_alumnos

---
+++ REVISION.md
@@ -0,0 +1,5 @@
+# Revsión del examen de DWES21 1er Trimestre
+
+## Errores
+- No borré el material de ejemplo
+- La vista no la creé en laravel, sólo en la base de datos. He creado la vista usando `DB::statement`.
---
+++ database/migrations/2021_12_14_144950_create_v_alumnos_view.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class CreateVAlumnosView extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement(
+            "CREATE VIEW v_alumnos AS
+             SELECT a.id, a.nombre, round(sum(n.nota)/count(n.nota), 1) AS nota_final
+               FROM alumnos a
+          LEFT JOIN notas n
+                 ON a.id = n.alumno_id
+           GROUP BY a.id, a.nombre"
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement(
+            "DROP VIEW IF EXISTS v_alumnos CASCADE"
+        );
+    }
+}
